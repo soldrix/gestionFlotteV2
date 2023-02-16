2fd00a981c6c190094ccc05b183c88c8fa3be79c
ethan gouillart <egouillart@litesoft.fr>
2023-02-16 17:32:30 +0100

    modification

--- resources/views/form/utilisateur/userCreate.blade.php
+++ resources/views/form/utilisateur/userCreate.blade.php
@@ -79,28 +79,6 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password ')}}<span class="text-danger">*</span>{{__(' :')}}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password ')}}<span class="text-danger">*</span>{{__(' :')}}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
                                 <label for="email_receiver" class="col-md-4 col-form-label text-md-end">{{ __('Envoyer l\'email à : ') }}</label>
 
                                 <div class="col-md-6">
