f0fb471766058f989fcec1b997d40e1034bcd86a
ethan gouillart <egouillart@litesoft.fr>
2023-02-17 11:59:56 +0100

    debut logout

--- app/Http/Middleware/SessionTimeout.php
+++ app/Http/Middleware/SessionTimeout.php
@@ -12,15 +12,24 @@ class SessionTimeout
 {
     public function handle(Request $request, Closure $next)
     {
-        // If user is not logged in...
-        if (!Auth::check()) {
-            return $next($request);
-        }
 
-        $userToken = Auth::user()->tokens->first();
+        if($request->wantsJson()){
+            if (!auth('sanctum')->check()) {
+                return $next($request);
+            }
+            $userToken = Auth('sanctum')->user()->tokens->first();
+
+        }else{
+            if (!auth()->check()) {
+                return $next($request);
+            }
+            $userToken = Auth::user()->tokens->first();
+        }
+        // If user is not logged in...
+
+
 
         $token = PersonalAccessToken::find($userToken->id);
-
         $now = Carbon::now();
 
         $last_seen = Carbon::parse($token->updated_at);
@@ -28,15 +37,21 @@ class SessionTimeout
         $absence = $now->diffInMinutes($last_seen);
 
         // If user has been inactivity longer than the allowed inactivity period
-        if ($absence > config('session.lifetime')) {
+        error_log(json_encode([
+            "debut" => date($userToken->updated_at),
+            "fin" => date('Y-m-d H:i:s'),
+            "other" => config('session.lifetime')
+            ]),JSON_PRETTY_PRINT);
+        if ($absence >= config('session.lifetime')) {
             $request->user()->tokens()->delete();
             $request->session()->invalidate();
             Auth::guard()->logout();
-            return redirect('login');
+            return ($request->wantsJson())? $next($request) : redirect('login');
         }
-
-        $token->updated_at = $now;
-        $token->save();
+        if(!$request->wantsJson()){
+            $token->updated_at = $now;
+            $token->save();
+        }
 
         return $next($request);
     }
